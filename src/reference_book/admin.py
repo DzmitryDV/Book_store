c0d9d3a30a59ce1ea6597d443fe35e2169ad19d4
DzmitryDV <mixvarb@gmail.com>
2022-10-09 22:49:38 +0300

    Added reference books

--- src/reference_book/admin.py
+++ src/reference_book/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from . import models
+
 # Register your models here.
+
+admin.site.register(models.BookAuthor)
+admin.site.register(models.BookGenre)
+admin.site.register(models.BookPublisher)
+admin.site.register(models.BookSerie)
--- src/reference_book/models.py
+++ src/reference_book/models.py
@@ -1,3 +1,48 @@
+from operator import mod
+from tabnanny import verbose
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
 # Create your models here.
+
+class BookAuthor(models.Model):
+    name = models.CharField(
+        max_length=100,
+        verbose_name="Автор",
+        
+        )
+    def __str__(self) -> str:
+        return self.name
+
+class BookSerie(models.Model):
+    name = models.CharField(
+        max_length=100,
+        verbose_name="Серия",
+        
+        )
+    def __str__(self) -> str:
+        return self.name
+
+class BookGenre(models.Model):
+    name = models.CharField(
+        max_length=100,
+        verbose_name="Жанр",
+        )
+    description = models.TextField(
+        blank=True,
+        null=True,
+    )
+    def __str__(self) -> str:
+        return self.name
+
+class BookPublisher(models.Model):
+    name = models.CharField(
+        max_length=100,
+        verbose_name="Издательство",
+        
+        )
+    def __str__(self) -> str:
+        return self.name
+
+
+
